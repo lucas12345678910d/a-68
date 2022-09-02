f6b192b900eb61e2732923eb072f94c3fcf841bc
lucas12345678910d <93794042+lucas12345678910d@users.noreply.github.com>
2022-09-02 17:12:20 -0300

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -13,8 +13,8 @@ export default class BottomTabNavigator extends Component {
       <NavigationContainer>
         <Tab.Navigator>
           
-          <TabScreen name="Alugue uma Bicicleta" component={RideScreen} />
-          <TabScreen name="Histórico de Locações" component={RideHistoryScreen} />
+          <Tab.Screen name="Alugue uma Bicicleta" component={RideScreen} />
+          <Tab.Screen name="Histórico de Locações" component={RideHistoryScreen} />
           
 
           {/*
